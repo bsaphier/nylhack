70adcba707e8383153734b54dbb68b6a6f7e4235
Benjamin Saphier <b.saphier@gmail.com>
2017-10-05 18:44:05 -0400

    server works if we need it...

--- server/api.js
+++ server/api.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 
 router.get('/running', (req, res) => res.send({ ok: true }));
-router.get('/hello', (req, res) => res.status(200));
+router.get('/hello', (req, res) => res.status(200).send('hello world'));
 
 // No routes matched? 404.
 router.use((req, res) => res.status(404).end());
