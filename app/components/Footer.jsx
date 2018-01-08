8f03e09430490e11f53ba42dd78d452ff90117e7
Benjamin Saphier <b.saphier@gmail.com>
2018-01-08 14:23:00 -0500

    rebuild but still not rendering on mobile...

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,12 +8,10 @@ import * as s from './footer.scss';
 const Footer = ({ page, changePage }) => (
     <div className={s.footer}>
 
-        {/* <Btns.NextBtn action={() => changePage(page - 1)}>Prev</Btns.NextBtn> */}
         <div className={`${s.iconBtn} ${s.left}`} onClick={() => changePage(page - 1)}>
             <img className={s.iconPrev} src={`./icons/back-01.svg`} />
         </div>
 
-        {/* <Btns.NextBtn action={() => changePage(page + 1)}>Next</Btns.NextBtn> */}
         <div className={`${s.iconBtn} ${s.right}`} onClick={() => console.log('We havent built this functionality yet.')}>
             <img className={s.iconMenu} src={`./icons/Menu.svg`} />
         </div>
